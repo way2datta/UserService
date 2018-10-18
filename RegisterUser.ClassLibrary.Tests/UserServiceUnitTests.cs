1a7c77d5dc015da3c0a2401916157a9e3577c7f2
Dattatray Kale <way2datta@gmail.com>
2018-10-18 17:09:54 +0530

    User service tests!

--- RegisterUser.ClassLibrary.Tests/UserServiceUnitTests.cs
+++ RegisterUser.ClassLibrary.Tests/UserServiceUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace RegisterUser.ClassLibrary.Tests
 {
@@ -6,26 +7,25 @@ namespace RegisterUser.ClassLibrary.Tests
     public class UserServiceUnitTests
     {
         private IUserService userService;
+        private Mock<INotificationSender> mockNotificationSender;
+        private Mock<IValidator<User>> mockUserValidator;
 
         [TestInitialize]
         public void Initialize()
         {
-            userService = new UserService(new EmailNotificationSender(), new UserValidator());
+            mockNotificationSender = new Mock<INotificationSender>();
+            mockUserValidator = new Mock<IValidator<User>>();
+
+            userService = new UserService(mockNotificationSender.Object,
+                                          mockUserValidator.Object);
         }
 
         [TestMethod]
         [ExpectedException(typeof(EntityNotValidException))]
-        public void CannotCreateUserWhenEmailIsNotProvided()
+        public void CannotCreateUserWhenDataIsNotProvided()
         {
-            var user = new User { Name = "John Doe" };
-            userService.Register(user);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(EntityNotValidException))]
-        public void CannotCreateUserWhenNameIsNotProvided()
-        {
-            var user = new User { Email = "John.Doe@test.com" };
+            var user = new User();
+            mockUserValidator.Setup(x => x.Validate(user)).Throws(new EntityNotValidException(""));
             userService.Register(user);
         }
 
@@ -39,11 +39,11 @@ namespace RegisterUser.ClassLibrary.Tests
             Assert.AreEqual(1, userService.GetUsers().Count, "Should have created a user");
         }
 
-        //[TestMethod]
-        //public void SendNotificationWhenNewUserIsCreated()
-        //{
-        //    var user = new User { Name = "John Doe", Email = "John.Doe@test.com" };
-        //    userService.Register(user);
-        //}
+        [TestMethod]
+        public void SendNotificationWhenNewUserIsCreated()
+        {
+            userService.Register(new User { Name="John Doe", Email="John.Doe@test.com"});
+            mockNotificationSender.Verify();
+        }
     }
 }
